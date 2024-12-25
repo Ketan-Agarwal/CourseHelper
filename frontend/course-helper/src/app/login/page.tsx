d234c2a7b718849057aff5c890cd53c3883cada4
Ketan <ketan.agarwal2506@gmail.com>
2024-12-25 18:15:13 +0530

    frontend__proto done

--- frontend/course-helper/src/app/login/page.tsx
+++ frontend/course-helper/src/app/login/page.tsx
@@ -1,9 +1,7 @@
 import LoginForm from "../ui/loginform";
-import styles from './login.module.css';
 
 export default function Page(){
     return(
-        <body className={styles.body}>
-        <LoginForm/></body>
+        <LoginForm/>
     )
 }
